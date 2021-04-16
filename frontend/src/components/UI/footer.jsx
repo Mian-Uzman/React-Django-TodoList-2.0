bfc5a391071aa33a784454e12c67a28c6ca96981
Mian Uzman <uzmana3@gmail.com>
2021-04-16 09:39:42 +0500

    footer changed

--- frontend/src/components/UI/footer.jsx
+++ frontend/src/components/UI/footer.jsx
@@ -8,40 +8,35 @@ function Footer() {
         <div className='footer'>
             <Card bg="dark" text="light">
                 <Card.Body>
-                    <Card.Title >Get things done from today !</Card.Title>
-                    <p>
+                    <Card.Title style={{ fontSize: '20px', textAlign: 'center' }}>Get things done from today !</Card.Title>
+                    <p style={{ fontWeight: '350', fontSize: '16px' }}>
                         About Me!
                 </p>
                     <ul className='footer-list'>
                         <button
                             type="button"
                             className="btn btn-outline-light">
-                            <a style={{ textDecoration: 'none', color: 'white' }} href='https://www.linkedin.com/in/mian-uzman-munib-b250501ba/'>LinkedIn</a>
+                            <a style={{ fontWeight: '350', textDecoration: 'none', color: 'white' }} href='https://www.linkedin.com/in/mian-uzman-munib-b250501ba/'>LinkedIn</a>
                         </button>
                         <button
                             type="button"
                             className="btn  btn-outline-light">
-                            <a style={{ textDecoration: 'none', color: 'white' }} href='https://www.instagram.com/mian__uzman/'>Instagram</a>
+                            <a style={{ fontWeight: '350', textDecoration: 'none', color: 'white' }} href='https://www.instagram.com/mian__uzman/'>Instagram</a>
                         </button>
                         <button
                             type="button"
                             className="btn  btn-outline-light">
-                            <a style={{ textDecoration: 'none', color: 'white' }} href='https://www.facebook.com/uzman.munib/'>Facebook</a>
+                            <a style={{ fontWeight: '350', textDecoration: 'none', color: 'white' }} href='https://www.facebook.com/uzman.munib/'>Facebook</a>
                         </button>
 
                     </ul>
 
-                    <Card.Text className="mt-5" >
+                    <Card.Text style={{ fontWeight: '350', fontSize: '16px' }} >
                         Contact Me!
                 </Card.Text>
-                    <button
-
-                        type="button"
-                        className="btn btn-light"
-                        href='https://www.facebook.com/uzman.munib/'>uzmanA3@gmail.com
-                        </button>
+                    <a>Email: <a style={{ fontWeight: '300', fontSize: '16px' }}>uzmanA3@gmail.com </a></a>
                 </Card.Body>
-                <Card.Footer style={{ textAlign: 'center' }} className="text-muted">Made By Uzman</Card.Footer>
+                <Card.Footer style={{ textAlign: 'center', fontSize: '14px' }} className="text-muted">Made By Uzman</Card.Footer>
             </Card>
         </div>
     )
